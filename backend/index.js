b37282b9c5252785fca3bbe71a904638cf6dca0a
ujjawal sharma <112757910+Sharmaujjawal123@users.noreply.github.com>
2025-09-28 16:33:18 +0530

    Clean up imports and modify root route response
    Removed unused imports and updated the root route response.

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,13 @@
 import express from "express";
-import fetch from "node-fetch";
-import multer from "multer";
-import cors from "cors";
 
-const app = express();
 
-// ✅ Use memory storage (no files on disk)
-// const upload = multer({ storage: multer.memoryStorage() });
 
-app.use(cors());
+
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
 
 
